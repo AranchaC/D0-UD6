c5cd86b2aa83e69dbeadb9472d2e397e80ec5550
AranchaC <aranchachicharro@gmail.com>
2024-01-18 14:14:52 +0100

    clase 18jan 14.14

---
+++ miniAPI_REST_Citas/src/main/java/daw/dwes/ud6/MiniApiRestCitasApplication.java
@@ -0,0 +1,13 @@
+package daw.dwes.ud6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniApiRestCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniApiRestCitasApplication.class, args);
+	}
+
+}
---
+++ miniAPI_REST_Citas/src/main/java/daw/dwes/ud6/QuoteRepository.java
@@ -0,0 +1,7 @@
+package daw.dwes.ud6;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuoteRepository extends JpaRepository<Quote, Long> {
+
+}
