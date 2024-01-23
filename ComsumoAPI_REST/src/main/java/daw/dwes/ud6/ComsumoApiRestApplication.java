1172165cb6a030fca2269aad0d446a8aca14ce62
AranchaC <aranchachicharro@gmail.com>
2024-01-23 13:14:24 +0100

    CLASE 23JAN 13.14 COMENTARIO PRUEBA

--- ComsumoAPI_REST/src/main/java/daw/dwes/ud6/ComsumoApiRestApplication.java
+++ ComsumoAPI_REST/src/main/java/daw/dwes/ud6/ComsumoApiRestApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
+// COMENTARIO DE PRUEBA
 
 @SpringBootApplication
 public class ComsumoApiRestApplication {
